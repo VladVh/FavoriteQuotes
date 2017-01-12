c5795d283410e0a19a6356d214ebbeb0dceac735
VladVh <vlad.voitsekh@yandex.ua>
2017-01-12 17:28:21 +0200

    working on preferences logic moved from activity to preferenceFragment

--- app/src/main/java/com/development/vvoitsekh/favoritequotes/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/development/vvoitsekh/favoritequotes/ui/settings/SettingsActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.util.DisplayMetrics;
+import android.util.Log;
 
 import com.development.vvoitsekh.favoritequotes.R;
 
@@ -18,7 +19,7 @@ import java.util.Locale;
  * Created by v.voitsekh on 06.01.2017.
  */
 
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
+public class SettingsActivity extends PreferenceActivity {
 
     public static Intent getStartIntent(Context context) {
         Intent intent = new Intent(context, SettingsActivity.class);
@@ -35,30 +36,63 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
 
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals(R.string.preference_language_key)) {
-            String lang = sharedPreferences.getString(key, "English");
-            Locale myLocale = new Locale(lang);
-            Resources res = getResources();
-            Configuration conf = res.getConfiguration();
-            DisplayMetrics dm = res.getDisplayMetrics();
-            conf.locale = myLocale;
-            res.updateConfiguration(conf, dm);
-            Intent refresh = new Intent(this, SettingsActivity.class);
-            startActivity(refresh);
-            finish();
-        } else if (key.equals(R.string.preference_notification_key)) {
+//    @Override
+//    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+//        if (key.equals(R.string.preference_language_key)) {
+//            String lang = sharedPreferences.getString(key, "English");
+//            Locale myLocale = new Locale(lang);
+//            Resources res = getResources();
+//            Configuration conf = res.getConfiguration();
+//            DisplayMetrics dm = res.getDisplayMetrics();
+//            conf.locale = myLocale;
+//            res.updateConfiguration(conf, dm);
+//            Intent refresh = new Intent(this, SettingsActivity.class);
+//            startActivity(refresh);
+//            finish();
+//        } else if (key.equals(R.string.preference_notification_key)) {
+//
+//        } else {
+//            Log.e("wtf", "another option");
+//        }
+//    }
 
-        }
-    }
-
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
         }
 
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals(getResources().getString(R.string.preference_language_key))) {
+                String lang = sharedPreferences.getString(key, "English");
+                Locale myLocale = new Locale(lang);
+                Resources res = getResources();
+                Configuration conf = res.getConfiguration();
+                DisplayMetrics dm = res.getDisplayMetrics();
+                conf.locale = myLocale;
+                res.updateConfiguration(conf, dm);
+                Intent refresh = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(refresh);
+                getActivity().finish();
+            } else if (key.equals(getResources().getString(R.string.preference_notification_key))) {
+
+            } else {
+                Log.e("wtf", "another option");
+            }
+        }
     }
 }
